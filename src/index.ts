b9a05a7015278fa0531cedc694c5e98c7a13ff62
nikolapajovic-bit <nikpajovic@gmail.com>
2026-01-25 18:16:04 +0100

    adding extensions for deployment

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ AgentAPI.config();
 
 import express from "express";
 import cors from "cors";
-import subjectsRouter from "./routes/subjects";
-import securityMiddleware from "./middleware/security";
+import subjectsRouter from "./routes/subjects.js";
+import securityMiddleware from "./middleware/security.js";
 import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = 8000;
